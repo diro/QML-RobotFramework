269ce9d13b6f0ead73f8cd2aa84d9f31f8247ecc
diro <diro.fan@gmail.com>
2015-03-10 17:10:21 +0800

    refine the GUI for label

--- LabelQMLComponent.js
+++ LabelQMLComponent.js
@@ -11,6 +11,6 @@ function removeObjectNameLabel(item) {
 function addObjectNameLabel(item) {
     for (var i = 0; i < item.children.length; i++) {
         addObjectNameLabel(item.children[i]);
-        var newObject = Qt.createQmlObject('import QtQuick 2.0; Text {objectName:"DYN_NAME_LABEL";font.pixelSize:12;color: "white"; height: 10;text:"'+ item.children[i].objectName + '";Rectangle{anchors.fill:parent;color:"black";border.width:1;border.color:"orange";opacity:0.4;z:-1}}', item.children[i]);
+        var newObject = Qt.createQmlObject('import QtQuick 2.0; Text {objectName:"DYN_NAME_LABEL";font.pixelSize:12;color: "white"; height: 10;text:"'+ item.children[i].objectName + '";Rectangle{height:parent.paintedHeight;width:parent.paintedWidth;color:"black";border.width:1;border.color:"orange";opacity:0.4;z:-1}}', item.children[i]);
     }
 }
